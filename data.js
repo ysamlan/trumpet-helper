cce323cfc34658618ccac5402162ac4c41a75f52
Yoni Samlan <ysamlan@gmail.com>
2025-04-04 12:56:31 -0400

    trumpet fingering data

--- data.js
+++ data.js
@@ -15,27 +15,44 @@
  * will need to handle equivalence if needed later.
  */
 const fingeringData = Object.freeze({
-    // --- Sample Data (C4 to G4) ---
-    "C4": { primary: [], alternates: [[1, 3], [1, 2, 3]] }, // Middle C (Open, Alt 1-3 or 1-2-3)
-    "C#4": { primary: [1, 2, 3], alternates: [[1, 2]] },   // C# / Db (1-2-3, Alt 1-2) - Note: [3] is often flat
-    "D4": { primary: [1, 3], alternates: [] },             // D (1-3)
-    "D#4": { primary: [2, 3], alternates: [] },             // D# / Eb (2-3)
-    "E4": { primary: [1, 2], alternates: [] },             // E (1-2)
-    "F4": { primary: [1], alternates: [] },                // F (1)
-    "F#4": { primary: [2], alternates: [[1, 3]] },         // F# / Gb (2, Alt 1-3)
-    "G4": { primary: [], alternates: [[1, 2]] },           // G (Open, Alt 1-2)
-
-    // --- To be expanded ---
-    // Example structure for notes outside the sample range:
-    // "F#3": { primary: [1, 2, 3], alternates: [] },
-    // "G3": { primary: [1, 3], alternates: [] },
-    // ...
-    // "A4": { primary: [1, 2], alternates: [] },
-    // "A#4": { primary: [1], alternates: [[2, 3]] },
-    // "B4": { primary: [2], alternates: [] },
-    // "C5": { primary: [1], alternates: [] },
-    // ...
-    // "C6": { primary: [1], alternates: [] }, // High C
+    "F#3": { primary: [1, 2, 3], alternates: [] },
+    "G3": { primary: [1, 3], alternates: [] },
+    "G#3": { primary: [2, 3], alternates: [] },
+    "A3": { primary: [1, 2], alternates: [] },
+    "A#3": { primary: [1], alternates: [] },
+    "B3": { primary: [2], alternates: [] },
+    "C4": { primary: [], alternates: [] },
+    "C#4": { primary: [1, 2, 3], alternates: [] },
+    "D4": { primary: [1, 3], alternates: [] },
+    "D#4": { primary: [2, 3], alternates: [] },
+    "E4": { primary: [1, 2], alternates: [] },
+    "F4": { primary: [1], alternates: [] },
+    "F#4": { primary: [2], alternates: [[1, 2, 3]] },
+    "G4": { primary: [], alternates: [[1, 3]] },
+    "G#4": { primary: [2, 3], alternates: [] },
+    "A4": { primary: [1, 2], alternates: [] },
+    "A#4": { primary: [1], alternates: [[1, 2, 3]] },
+    "B4": { primary: [2], alternates: [[1, 3]] },
+    "C5": { primary: [], alternates: [[2, 3]] },
+    "C#5": { primary: [1, 2], alternates: [[1, 2, 3]] },
+    "D5": { primary: [1], alternates: [[1, 3]] },
+    "D#5": { primary: [2], alternates: [[2, 3]] },
+    "E5": { primary: [], alternates: [[1, 2, 3], [1, 2]] },
+    "F5": { primary: [1], alternates: [[1, 3]] },
+    "F#5": { primary: [2], alternates: [[1, 2, 3], [2, 3]] },
+    "G5": { primary: [], alternates: [[1, 3], [1, 2]] },
+    "G#5": { primary: [2, 3], alternates: [[1], [1, 2, 3]] },
+    "A5": { primary: [1, 2], alternates: [[1, 3]] },
+    "A#5": { primary: [1], alternates: [[1, 2, 3], [2, 3], []] },
+    "B5": { primary: [2], alternates: [[1, 3], [1, 2]] },
+    "C6": { primary: [], alternates: [[1], [2, 3], [1, 2, 3]] },
+    "C#6": { primary: [2], alternates: [[1, 2], [1, 3], [1, 2, 3]] },
+    "D6": { primary: [], alternates: [[1], [2, 3]] },
+    "D#6": { primary: [2], alternates: [[1, 2]] },
+    "E6": { primary: [], alternates: [[1]] },
+    "F6": { primary: [1], alternates: [[2]] },
+    "F#6": { primary: [2], alternates: [[]] },
+    "G6": { primary: [], alternates: [] }
 });
 
 /**
